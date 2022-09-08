ea457eb79ff40ebb4a95c595f62af6b72cd0743c
sophia pung <sophiagracepung@gmail.com>
2022-09-08 09:51:35 -0700

    wip API

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 // this is the server index 
 import express from 'express';
 import cors from 'cors';
+import Server from './server';
+import fetch from "node-fetch";
+
+import { config } from "dotenv";
+config();
+
+console.log(`Your api key is ${process.env.API_KEY}`);
 
 const app = express();
 const PORT = 8090; 
@@ -20,3 +27,28 @@ app.get('/api/outfits', (req, res) => {
     ]
     res.json(OUTFIT)
 })
+
+app.get("/weather", (req, res) => {
+    const city = req.query.cityName;
+    const apiKey = process.env.API_KEY;
+    const params = new URLSearchParams({
+      q: req.query.cityName,
+      appid: process.env.API_KEY,
+      units: "imperial",
+    });
+    const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        res.send({ data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+// console.log that your server is up and running
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
